b80227c0d9f9a5b360f5979be1facbd2603e6cb0
Francisco Olmos <francisco.olmos.ubp@gmail.com>
2020-04-20 10:33:05 -0300

    Se agrega el punto 5 de la ejercitacion

---
+++ Route/creditos.js
@@ -0,0 +1,13 @@
+
+const creditos =  {
+    creditosTM: function(req,res) {
+        const text = 
+        `<p>
+        Lorem100
+        </p>
+        `;
+        res.send(text);
+    }
+}
+
+module.exports = creditos;
